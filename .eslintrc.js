f316c76d6e81985365c4a2a295a72a130a39aadb
Frank Deng <frank.deng@tinklabs.com>
2019-01-05 11:26:50 +0800

    Update eslint config.

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,5 +7,14 @@ module.exports = {
   parserOptions: {
     parser: 'babel-eslint'
   },
-  extends: ['airbnb-base', 'plugin:vue/recommended']
+  extends: ['airbnb-base', 'plugin:vue/recommended'],
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-alert": process.env.NODE_ENV === "production" ? "error" : "off",
+    //强制使用单引号
+    quotes: ["error", "single"],
+    //强制不使用分号结尾
+    semi: ["error", "never"]
+  },
 }
