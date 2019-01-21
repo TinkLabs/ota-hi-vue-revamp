06c72d6fc7e9525cc872b73a42c2892025bf9703
vino <vino.wu@tinklabs.com>
2019-01-21 18:08:17 +0800

    add form validate

--- src/common/component/formValidate.js
+++ src/common/component/formValidate.js
@@ -1,16 +1,38 @@
 export default {
   install(Vue, option) {
+    const objectVue = Vue
     const $t = option.i18n
-    Vue.prototype.$v = {
-      onlyLetterSpace: (rule, value, callback) => {
-        console.log(this)
-        if (!/^[a-zA-Z][a-zA-Z\s]*$/.test(value)) {
-          callback(new Error($t('Please use character (A-Z, a-z) values only')))
-        } else {
-          callback()
-        }
-        return null
-      },
+    const onlyLetterSpace = (rule, value, callback) => {
+      if (!/^[a-zA-Z][a-zA-Z\s]*$/.test(value)) {
+        callback(new Error($t('Please use character (A-Z, a-z) values only')))
+      } else {
+        callback()
+      }
+      return null
+    }
+    objectVue.prototype.$v = {
+      firstName: [
+        {
+          required: true,
+          message: $t('Please Input Your First Name'),
+          trigger: 'blur',
+        },
+        {
+          validator: onlyLetterSpace,
+          trigger: 'blur',
+        },
+      ],
+      lastName: [
+        {
+          required: true,
+          message: $t('Please Input Your Last Name'),
+          trigger: 'blur',
+        },
+        {
+          validator: onlyLetterSpace,
+          trigger: 'blur',
+        },
+      ],
     }
   },
 }
