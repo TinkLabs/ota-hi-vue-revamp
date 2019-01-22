17536c6bff0a0bf3bad8919da2ad150e0d462996
zhangyu <yu.zhang@tinklabs.com>
2019-01-22 15:41:35 +0800

    PC homepage style

--- src/desktop/views/includes/header.vue
+++ src/desktop/views/includes/header.vue
@@ -564,6 +564,7 @@ export default {
     },
     // searchbar fixed
     handleScroll() {
+      console.log('scroll')
       const eleScrollTop = document.documentElement.scrollTop
       const scrollTop = window.pageYOffset || eleScrollTop || document.body.scrollTop
       const [offsetTop] = [document.querySelector('.search-bar').offsetTop]
@@ -711,7 +712,7 @@ export default {
         li{
           padding:10px 20px;
           color:#505050;
-          width:200px;
+          width:210px;
           &:hover{
             color:#fff;
             background-color:#002b55;
@@ -894,7 +895,7 @@ export default {
       .guests {
         position: relative;
         .guest-num {
-          min-width: 140px;
+          min-width: 180px;
           height: 72px;
           background-color:#ebebeb;
           border-radius: 5px;
